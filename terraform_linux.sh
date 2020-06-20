57ffdd5c131b1b9d251b8aa1266e00b64038de08
Guilherme Ortiz <code@guilhermeortiz.net>
2020-06-20 19:17:59 -0300

    Update status

--- terraform_linux.sh
+++ terraform_linux.sh
@@ -1,4 +1,9 @@
+#!/bin/bash
+echo "Downloading Terraform..."
 wget https://releases.hashicorp.com/terraform/0.12.26/terraform_0.12.26_linux_amd64.zip
+echo "Unziping..."
 unzip terraform_0.12.26_linux_amd64.zip
+echo "Moving to /bin"
 sudo mv terraform /bin
+echo "All done!"
 terraform -version
