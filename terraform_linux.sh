b64d7e4b2afa9b4fec0a1fc96b73a49207824009
Guilherme Ortiz <code@guilhermeortiz.net>
2020-06-20 19:38:47 -0300

    Update script

--- terraform_linux.sh
+++ terraform_linux.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
+
+should_remove_previous_version() {
+  if [[ $(terraform -version) ]]; then
+    echo "Removing previous terraform..."
+    sudo rm /bin/terraform
+  fi
+}
+
+should_remove_previous_version
 echo "Downloading Terraform..."
 wget https://releases.hashicorp.com/terraform/0.12.26/terraform_0.12.26_linux_amd64.zip
 echo "Unziping..."
 unzip terraform_0.12.26_linux_amd64.zip
 echo "Moving to /bin"
-sudo mv terraform /bin
+mv terraform /bin
+echo "Cleaning files.."
+rm terraform_0.12.26_linux_amd64.zip
 echo "All done!"
 terraform -version
