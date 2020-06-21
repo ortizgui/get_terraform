32c0fd892806c21f172a0eeb31fb7c655ac5182f
Guilherme Ortiz <code@guilhermeortiz.net>
2020-06-20 22:27:18 -0300

    check unzip package

--- terraform_linux.sh
+++ terraform_linux.sh
@@ -10,10 +10,22 @@ remove_previous_version()
     fi
 }
 
+has_unzip_package()
+{
+    unzip -version
+    if [ $? -eq 0] ; then
+        echo "Unzip package already exist..."
+    else
+        apt-get update
+        apt-get install unzip
+    fi
+}
+
 remove_previous_version
 echo "Downloading Terraform..."
 wget https://releases.hashicorp.com/terraform/0.12.26/terraform_0.12.26_linux_amd64.zip
 echo "Unziping..."
+has_unzip_package
 unzip terraform_0.12.26_linux_amd64.zip
 echo "Moving to /bin"
 sudo mv terraform /bin
