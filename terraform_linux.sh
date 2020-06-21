c3a60bda78d562eaced8910cc3eedb868ef4dc02
Guilherme Ortiz <code@guilhermeortiz.net>
2020-06-20 21:08:42 -0300

    trying function

--- terraform_linux.sh
+++ terraform_linux.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+TERRAFORM=/bin/terraform
+
+remove_previous_version()
+{
+    if test -f "$TERRAFORM"; then
+        echo "Removing previous version"
+        sudo rm "$TERRAFORM"
+    fi
+}
+
+remove_previous_version
 echo "Downloading Terraform..."
 wget https://releases.hashicorp.com/terraform/0.12.26/terraform_0.12.26_linux_amd64.zip
 echo "Unziping..."
